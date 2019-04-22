5e9f435af32dbb8f6283d7a8ad1381fccbe37b7c
moriyama <kenshumoriyama01@gmail.com>
2019-04-22 11:25:24 +0900

    ver.2

--- enshu_sample.rb
+++ enshu_sample.rb
@@ -3,7 +3,11 @@ def ch_card(t_ch,tn,tnn)
         tn = tnn
     end
 end
-
+t1_ch = 0
+t2_ch = 0
+t3_ch = 0
+t4_ch = 0
+t5_ch = 0
 win = 0
 lose = 0
 ary_win_card = []
@@ -13,7 +17,7 @@ while(1)
 
         p "1.ゲームの開始"
         p "2.成績画面"
-        p "6.終了"
+        p "0.終了"
 
         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         sel = gets.to_i
@@ -21,10 +25,15 @@ while(1)
         when 1 then
             while(1)
                 t1 = "[s1]"
+                t1_disp = t1
                 t2 = "[c2]"
+                t2_disp = t2
                 t3 = "[h3]"
+                t3_disp = t3
                 t4 = "[d5]"
+                t4_disp = t4
                 t5 = "[s6]"
+                t5_disp = t5
                 t6 = "[c7]"
                 t7 = "[h9]"
                 t8 = "[d10]"
@@ -54,25 +63,75 @@ while(1)
                                 puts "5:#{t5}を交換する"
                                 puts "0:カードの交換を終了する"
                                 ans_game_trade = gets.to_i
-                                t1_ch = 0
-                                t2_ch = 0
-                                t3_ch = 0
-                                t4_ch = 0
-                                t5_ch = 0
-                            end
+                            
                                 case ans_game_trade
                                     when 0 then
                                         puts "本当によろしいですか？"
                                         puts "0でカードの交換を終了します。0以外で継続します"
                                         ans_game_trade_end = gets.to_i
-                                        if ans_game_trade_end == 0
+                                        if ans_game_trade_end == 0 then
                                             ans_game_trade = 0
+                                        else
+                                            ans_game_trade = 10
                                         end
-                                        ch_card(t1_ch,t1,t6)
-                                        ch_card(t2_ch,t2,t7)
-                                        ch_card(t3_ch,t3,t8)
-                                        ch_card(t4_ch,t4,t9)
-                                        ch_card(t5_ch,t5,t10)
+                                    when 1 then
+                                        if t1_ch == 0 then
+                                            t1_ch = 1
+                                            t1 = t1 + "●"
+                                        else
+                                            t1_ch = 0
+                                            t1 = t1_disp
+                                        end
+                                    when 2 then
+                                        if t2_ch == 0 then
+                                            t2_ch = 1
+                                            t2 = t2 + "●"
+                                        else
+                                            t2_ch = 0
+                                            t2 = t2_disp
+                                        end
+                                    when 3 then
+                                        if t3_ch == 0 then
+                                            t3_ch = 1
+                                            t3 = t3 + "●"
+                                        else
+                                            t3_ch = 0
+                                            t3 = t3_disp
+                                        end
+                                    when 4 then
+                                        if t4_ch == 0 then
+                                            t4_ch = 1
+                                            t4 = t4 + "●"
+                                        else
+                                            t4_ch = 0
+                                            t4 = t4_disp
+                                        end
+                                    when 5 then
+                                        if t5_ch == 0 then
+                                            t5_ch = 1
+                                            t5 = t5 + "●"
+                                        else
+                                            t5_ch = 0
+                                            t5 = t5_disp
+                                        end
+                                    else
+                                end
+                            end
+                                if t1_ch == 1
+                                    t1 = t6
+                                end
+                                if t2_ch == 2
+                                    t2 = t7
+                                end
+                                if t3_ch == 3
+                                    t3 = t8
+                                end
+                                if t4_ch == 4
+                                    t4 = t9
+                                end
+                                if t5_ch == 1
+                                    t5 = t10
+                                end
                                         puts "~~~~~~~~~~~~勝敗画面~~~~~~~~~~~~"
                                         card = 0
                                         if (t1 == "[s1]" && t5 == "[c1]")
@@ -95,56 +154,17 @@ while(1)
                                             lose += 1
                                             p "戦績:#{win}勝#{lose}敗"
                                         end
+                                        
+                                        t1_ch = 0
+                                        t2_ch = 0
+                                        t3_ch = 0
+                                        t4_ch = 0
+                                        t5_ch = 0
+                                
                                         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
                                         p "続けますか? 0でメインメニュー、0以外で続けます"
                                         tuduki = gets.to_i
-                                    when 1 then
-                                        t1_disp = t1
-                                        if t1_ch == 0 then
-                                            t1_ch = 1
-                                            t1 << "●"
-                                        else
-                                            t1_ch = 0
-                                            t1 = t1_disp
-                                        end
-                                    when 2 then
-                                        t2_disp = t2
-                                        if t2_ch == 0 then
-                                            t2_ch = 1
-                                            t2 << "●"
-                                        else
-                                            t2_ch = 0
-                                            t2 = t2_disp
-                                        end
-                                    when 3 then
-                                        t3_disp = t3
-                                        if t3_ch == 0 then
-                                            t3_ch = 1
-                                            t3 << "●"
-                                        else
-                                            t3_ch = 0
-                                            t3 = t3_disp
-                                        end
-                                    when 4 then
-                                        t4_disp = t4
-                                        if t4_ch == 0 then
-                                            t4_ch = 1
-                                            t4 << "●"
-                                        else
-                                            t4_ch = 0
-                                            t4 = t4_disp
-                                        end
-                                    when 5 then
-                                        t5_disp = t5
-                                        if t5_ch == 0 then
-                                            t5_ch = 1
-                                            t5 << "●"
-                                        else
-                                            t5_ch = 0
-                                            t5 = t5_disp
-                                        end
-                                    
-                                end
+
                             if tuduki == 0 then                    
                                 break
                             end
