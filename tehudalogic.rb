8d5f91b5b88a4052e3b80a18782fdd74844b29f7
石河優貴 <burle42820@gmail.com>
2019-04-23 15:21:35 +0900

    NPCのドローメソッドとりあえず実装

--- tehudalogic.rb
+++ tehudalogic.rb
@@ -11,11 +11,7 @@ class Pokerdraw
     def hudajunbi()
         deck = []
         $dbh.do("DROP TABLE IF EXISTS card")
-        $dbh.do("CREATE TABLE card(id integer primary key,number integer,suit char(20))") 
-        $dbh.do("DROP TABLE IF EXISTS mcard")
-        $dbh.do("CREATE TABLE mcard(id integer primary key,mcard )") 
-        $dbh.do("DROP TABLE IF EXISTS ncard")
-        $dbh.do("CREATE TABLE ncard(id integer primary key,ncard )") 
+        $dbh.do("CREATE TABLE card(id integer primary key,suit char(20),number integer)") 
         #トランプ全部つっこむとこ
         1.upto(13){|i|
         $dbh.do("insert into card(number,suit) VALUES(?,?)",i,"D")
@@ -24,27 +20,31 @@ class Pokerdraw
         $dbh.do("insert into card(number,suit) VALUES(?,?)",i,"C")
         }
         $dbh.select_all("select number,suit from card"){|crd|
-            deck << [crd[1],crd[0]]
+            deck << [crd[0],crd[1]]
         }
         return deck
     end
 
-    def fulldraw()
+    def fulldraw(name)
         fb = []
         dc = []
+        $dbh.do("DROP TABLE IF EXISTS #{name}")
+        $dbh.do("CREATE TABLE #{name}(id integer primary key,suit char(20),number integer)") 
         #トランプ抜くとこ
         5.times{|j|
             while(1)
                 r = rand(52)
                 $dbh.select_all("select number,suit from card where id = ?",r){|crd|
-                    dc = crd[1],crd[0]
+                    dc = crd[0],crd[1]
                 }
                 if dc != nil
+                    $dbh.do("insert into #{name}(number,suit) values(?,?)",dc[0],dc[1])
                     break
                 end
             end
             fb << dc
-            $dbh.do("delete from card where id = ?",r)
+            $dbh.do("delete from card where id = ?",r) 
+
         }
         return fb
     end
@@ -67,6 +67,32 @@ class Pokerdraw
         $dbh.do("delete from card where id = ?",r)
         return sb
     end
+    def npcchoice(drw)
+        pdw = 0
+        tdp = 0
+        $dbh.select_all("select count(number),number from ncard group by number having 1<count(*)"){|npcard|
+        #重複している数
+                pdw = npcard[0].to_i
+        }
+        if pdw > 1
+            if pdw == 4
+                return drw
+            end
+            $dbh.select_all("select number from ncard group by number having 1<count(number)"){|lll|
+                #重複してるカードの等級
+                tdp = lll[0].to_i
+            }
+            5.times{|l|
+                if drw[l] != tdp
+                    drw[l] = draw()
+                else
+                    drw[l] = drw[l]
+                end
+            }
+        else
+            drw = fulldraw("ncard")
+        end
+    end 
 end
 
 
@@ -92,7 +118,8 @@ p s
 
 puts ""
 
-s =[pd.hudajunbi(),pd.fulldraw(),pd.fulldraw()]
+s =[pd.hudajunbi(),pd.fulldraw("ncard"),pd.fulldraw("mcard")]
+
 
 p s
 
@@ -107,12 +134,38 @@ puts t[0]
 trw = pd.draw()
 tlw = pd.draw()
 
-tcw = trw[1] + tlw[1]
+test = []
+test = pd.fulldraw("ncard")
+puts "jaberunulualalalalala"
+p test
+puts"bananaajilililili"
+puts ""
 
-p tcw
-
-npc = pd.fulldraw()
+pd.npcchoice(test)
 
 
+puts "gaba"
+p test
+puts "GABA"
 
 
+#p pdw
+#if pdw ==4 
+#    break
+#    else
+ #       if pdw <=2
+  #          5.times{|i|
+#
+ #           p chanf
+  #          }
+#
+
+
+#puts "kusozako"
+#puts tdp
+#puts "gabagaba"
+
+#p pdw
+#tcw = "trw[1]"+"tlw[1]"
+
+#p tcw
